cb8e22cd4f74a915ac303468f556280cf21260fe
CuongMinh72 <CuongMinh72@users.noreply.github.com>
2024-02-21 10:54:25 +0700

    add post method

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+app.use(express.json());
 
 require('dotenv/config');
 
@@ -16,6 +17,7 @@ app.get(api+'/products', (req, res) =>{
 });
 
 app.post(api+'/products', (req, res) =>{
+    app.use(express.json());
     const newProduct = req.body;
     console.log(newProduct);
     res.send(newProduct);
